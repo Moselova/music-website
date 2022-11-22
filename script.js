7487e3f79b456df198a3ca54432f974a4bf566c4
moses faye <104700694+Moselova@users.noreply.github.com>
2022-11-22 21:06:44 +0100

    trying to make the song play and some other changes

---
+++ script.js
@@ -0,0 +1,48 @@
+function openNav() {
+	document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+	document.getElementById("myNav").style.width = "0%";
+}
+
+const footerDiv = document.getElementById('footer-div')
+const playBtn = document.querySelector("#play");
+const prevBtn = document.querySelector('#prev')
+const nextBtn = document.querySelector("#next");
+const audio = document.querySelector("#audio");
+const progress = document.querySelector('#progress')
+const progressContainer = document.querySelector('progress-container')
+const title = document.getElementById('title')
+const cover = document.querySelector('#cover')
+
+// song titles
+const songs = ['nkan nbe', 'nwa-baby', 'thuggin']
+ let songIndex = 2
+ loadSong(songs[songIndex])
+
+ function playSong(params) {
+	footer-div.classList.add('play')
+ }
+ function pauseSong(params) {
+	
+ }
+
+//  to update song details
+function loadSong(song) {
+	title.innerText = song
+	audio.src = `songs/${'song'}.mp3`;
+	cover.src = `images/${'song'}.jpg`;
+}
+
+
+
+// event listeners
+playBtn.addEventListener('click', () => {
+  const isPlaying = footer-div.classList.contains('play');
+  if(isPlaying) {
+	pauseSong()
+  } else {
+	playSong()
+  }
+})
